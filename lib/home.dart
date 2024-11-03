d66a0feb67f0e35b2821cde60da926993165f9dd
Youssef <ym067595@gmail.com>
2024-11-03 20:41:57 +0200

    init project

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
